20d4b7709a6902305fcd90562cf4936d301c04e3
Huber Bastian <huber@huberlaptop.(none)>
2010-11-10 20:09:05 +0100

    multiple measurements

--- Blatt02/basdl.c
+++ Blatt02/basdl.c
@@ -7,6 +7,7 @@
 #include "omp.h"
 #include <common.h>
 
+#define MEASURES 10
 #define GV_SCALE(a, amin, amax, nmin, nmax) \
 		(( \
 			( (a) - (amin) ) * ( (nmax) - (nmin) ) + ( (amax) - (amin) ) / 2 \
@@ -40,7 +41,7 @@ int mpi_np;
 int mpi_self;
 
 /* Time measurements */
-double t_load, t_scatter, t_reduction, t_scale, t_gather;
+double t_load[MEASURES], t_scatter[MEASURES], t_reduction[MEASURES], t_scale[MEASURES], t_gather[MEASURES];
 
 /*
  * Implementations
@@ -76,157 +77,157 @@ int main(int argc, char *argv[]) {
 	int rows, columns, maxcolor;
 	int* image;
 	
-	int amin, amax;
-	switch(conv) {
-		case CONV_SEQUENTIAL:
-			printf("Convert Sequential...\n");
-			t_scatter = t_gather = 0.0;
-			
-			t_load = seconds();
-			image = ppp_pnm_read(input, &kind, &rows, &columns, &maxcolor);
-			t_load = seconds() - t_load;
-			
-			t_reduction = seconds();
-			sequential_min_max_grayvals(image, rows, columns, maxcolor, &amin, &amax);
-			t_reduction = seconds() - t_reduction;
-			
-			t_scale = seconds();
-			sequential_scale(image, rows, columns, maxcolor,
-				amin, amax, nmin, nmax);
-			t_scale = seconds() - t_scale;
-			
-			ppp_pnm_write(output, kind, rows, columns, maxcolor, image);
-			
-			printf("Load:\t\t%f\nMin/Max:\t%f\nScale:\t\t%f\n",
-				t_load, t_reduction, t_scale);
-		break;
-		
-		case CONV_OPENMP:
-			printf("Convert Openmp...\n");
-			t_scatter = t_gather = 0.0;
-			
-			t_load = seconds();
-			image = ppp_pnm_read(input, &kind, &rows, &columns, &maxcolor);
-			t_load = seconds() - t_load;
-			
-			t_reduction = seconds();
-			openmp_min_max_grayvals(image, rows, columns, maxcolor, &amin, &amax);
-			t_reduction = seconds() - t_reduction;
-			
-			
-			t_scale = seconds();
-			openmp_scale(image, rows, columns, maxcolor,
-				amin, amax, nmin, nmax);
-			t_scale = seconds() - t_scale;
-			
-			ppp_pnm_write(output, kind, rows, columns, maxcolor, image);
-			
-			printf("Load:\t\t%f\nMin/Max:\t%f\nScale:\t\t%f\n",
-				t_load, t_reduction, t_scale);
-		break;
-		
-		case CONV_MPI: {
-			MPI_Init(&argc, &argv);
-			MPI_Comm_size(MPI_COMM_WORLD, &mpi_np);
-			MPI_Comm_rank(MPI_COMM_WORLD, &mpi_self);
-			int i; // Common iteration variable
-			
-			/*
-			 * Read the image if current is the master...
-			 */
-			if(mpi_self == 0) {
-				t_load = seconds();
-				image = ppp_pnm_read(input, &kind, &rows, &columns, &maxcolor);
-				t_load = seconds() - t_load;
-			}
-			
-			/*
-			 * Broadcast the size of the image, as well as the maxcolor
-			 */
-			int image_meta[] = {rows, columns, maxcolor};
-			int counts[mpi_np], displs[mpi_np];
-			MPI_Bcast(image_meta, 3, MPI_INT, 0, MPI_COMM_WORLD);
-			
-			
-			/*
-			 * Prepare parts for scatterv
-			 */
-			int partSize = (image_meta[0]*image_meta[1])/mpi_np;
-			for(i = 0; i < mpi_np; i++) {
-				counts[i] = partSize;
-				displs[i] = partSize*i;
-			}
-			counts[mpi_np-1] = (image_meta[0]*image_meta[1])%partSize;
-			counts[mpi_np-1] = counts[mpi_np-1]==0?partSize:counts[mpi_np-1];
-			
-			/*
-			 * Allocate space for the image on receiving side...
-			 */
-			if(mpi_self != 0) {
-				image = (int*)malloc(partSize*sizeof(int));
-			}
-			
-			/*
-			 * Scatter data...
-			 */
-			MPI_Barrier(MPI_COMM_WORLD);
-			t_scatter = seconds();
-			MPI_Scatterv(image, counts, displs, MPI_INT, image, counts[mpi_self], MPI_INT, 0, MPI_COMM_WORLD);
-			MPI_Barrier(MPI_COMM_WORLD);
-			t_scatter = seconds() - t_scatter;
-			
-			/*
-			 * Calculate max...
-			 */
-			int max, min;
-			max = 0;
-			min = maxcolor;
-			for(i = 0; i < counts[mpi_self]; i++) {
-				max = MAX(max, image[i]);
-				min = MIN(min, image[i]);
-			}
-			
-			int g_max, g_min;
-			MPI_Barrier(MPI_COMM_WORLD);
-			t_reduction = seconds();
-			MPI_Allreduce(&max, &g_max, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
-			MPI_Allreduce(&min, &g_min, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
-			MPI_Barrier(MPI_COMM_WORLD);
-			t_reduction = seconds() - t_reduction;
-
-			/*
-			 * Now, as everybody has the min and max, scale the values locally
-			 */
-			t_scale = seconds();
-			for (i = 0; i < counts[mpi_self]; i++) {
-				image[i] = GV_SCALE(image[i], g_min, g_max, nmin, nmax);
-			}
-			t_scale = seconds() - t_scale;
-			
-			/*
-			 * Gather the image again...
-			 */
-			MPI_Barrier(MPI_COMM_WORLD);
-			t_gather = seconds();
-			MPI_Gatherv(image, counts[mpi_self], MPI_INT, image, counts, displs, MPI_INT, 0, MPI_COMM_WORLD);
-			MPI_Barrier(MPI_COMM_WORLD);
-			t_gather = seconds() - t_gather;
-			
-			if(mpi_self == 0) {
-				ppp_pnm_write(output, kind, rows, columns, maxcolor, image);
-				printf("Load:\t\t%f\nScatter:\t%f\nReduction:\t%f\nScale:\t\t%f\nGather:\t\t%f\n",
-					t_load, t_scatter, t_reduction, t_scale, t_gather);
-			}
-			
-			/* MPI beenden */
-			MPI_Finalize();
-		break;}
-		
-		default:
-		break;
-	}
+	int amin, amax, measurepos;	
+	for(measurepos = 0; measurepos < MEASURES; measurepos++) {
 	
+		switch(conv) {
+			case CONV_SEQUENTIAL:
+				printf("Convert Sequential...\n");
+				t_scatter[measurepos] = t_gather[measurepos] = 0.0;
+			
+				t_load[measurepos] = seconds();
+				image = ppp_pnm_read(input, &kind, &rows, &columns, &maxcolor);
+				t_load[measurepos] = seconds() - t_load[measurepos];
+			
+				t_reduction[measurepos] = seconds();
+				sequential_min_max_grayvals(image, rows, columns, maxcolor, &amin, &amax);
+				t_reduction[measurepos] = seconds() - t_reduction[measurepos];
+			
+				t_scale[measurepos] = seconds();
+				sequential_scale(image, rows, columns, maxcolor,
+					amin, amax, nmin, nmax);
+				t_scale[measurepos] = seconds() - t_scale[measurepos];
+			
+				ppp_pnm_write(output, kind, rows, columns, maxcolor, image);
+			break;
+		
+			case CONV_OPENMP:
+				printf("Convert Openmp...\n");
+				t_scatter[measurepos] = t_gather[measurepos] = 0.0;
+			
+				t_load[measurepos] = seconds();
+				image = ppp_pnm_read(input, &kind, &rows, &columns, &maxcolor);
+				t_load[measurepos] = seconds() - t_load[measurepos];
+			
+				t_reduction[measurepos] = seconds();
+				openmp_min_max_grayvals(image, rows, columns, maxcolor, &amin, &amax);
+				t_reduction[measurepos] = seconds() - t_reduction[measurepos];
+			
+			
+				t_scale[measurepos] = seconds();
+				openmp_scale(image, rows, columns, maxcolor,
+					amin, amax, nmin, nmax);
+				t_scale[measurepos] = seconds() - t_scale[measurepos];
+			
+				ppp_pnm_write(output, kind, rows, columns, maxcolor, image);
+			break;
+		
+			case CONV_MPI: {
+				MPI_Init(&argc, &argv);
+				MPI_Comm_size(MPI_COMM_WORLD, &mpi_np);
+				MPI_Comm_rank(MPI_COMM_WORLD, &mpi_self);
+				int i; // Common iteration variable
+			
+				/*
+				 * Read the image if current is the master...
+				 */
+				if(mpi_self == 0) {
+					t_load[measurepos] = seconds();
+					image = ppp_pnm_read(input, &kind, &rows, &columns, &maxcolor);
+					t_load[measurepos] = seconds() - t_load[measurepos];
+				}
+			
+				/*
+				 * Broadcast the size of the image, as well as the maxcolor
+				 */
+				int image_meta[] = {rows, columns, maxcolor};
+				int counts[mpi_np], displs[mpi_np];
+				MPI_Bcast(image_meta, 3, MPI_INT, 0, MPI_COMM_WORLD);
+			
+			
+				/*
+				 * Prepare parts for scatterv
+				 */
+				int partSize = (image_meta[0]*image_meta[1])/mpi_np;
+				for(i = 0; i < mpi_np; i++) {
+					counts[i] = partSize;
+					displs[i] = partSize*i;
+				}
+				counts[mpi_np-1] = (image_meta[0]*image_meta[1])%partSize;
+				counts[mpi_np-1] = counts[mpi_np-1]==0?partSize:counts[mpi_np-1];
+			
+				/*
+				 * Allocate space for the image on receiving side...
+				 */
+				if(mpi_self != 0) {
+					image = (int*)malloc(partSize*sizeof(int));
+				}
+			
+				/*
+				 * Scatter data...
+				 */
+				MPI_Barrier(MPI_COMM_WORLD);
+				t_scatter[measurepos] = seconds();
+				MPI_Scatterv(image, counts, displs, MPI_INT, image, counts[mpi_self], MPI_INT, 0, MPI_COMM_WORLD);
+				MPI_Barrier(MPI_COMM_WORLD);
+				t_scatter[measurepos] = seconds() - t_scatter[measurepos];
+			
+				/*
+				 * Calculate max...
+				 */
+				int max, min;
+				max = 0;
+				min = maxcolor;
+				for(i = 0; i < counts[mpi_self]; i++) {
+					max = MAX(max, image[i]);
+					min = MIN(min, image[i]);
+				}
+			
+				int g_max, g_min;
+				MPI_Barrier(MPI_COMM_WORLD);
+				t_reduction[measurepos] = seconds();
+				MPI_Allreduce(&max, &g_max, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+				MPI_Allreduce(&min, &g_min, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+				MPI_Barrier(MPI_COMM_WORLD);
+				t_reduction[measurepos] = seconds() - t_reduction[measurepos];
 
+				/*
+				 * Now, as everybody has the min and max, scale the values locally
+				 */
+				t_scale[measurepos] = seconds();
+				for (i = 0; i < counts[mpi_self]; i++) {
+					image[i] = GV_SCALE(image[i], g_min, g_max, nmin, nmax);
+				}
+				t_scale[measurepos] = seconds() - t_scale[measurepos];
+			
+				/*
+				 * Gather the image again...
+				 */
+				MPI_Barrier(MPI_COMM_WORLD);
+				t_gather[measurepos] = seconds();
+				MPI_Gatherv(image, counts[mpi_self], MPI_INT, image, counts, displs, MPI_INT, 0, MPI_COMM_WORLD);
+				MPI_Barrier(MPI_COMM_WORLD);
+				t_gather[measurepos] = seconds() - t_gather[measurepos];
+			
+				if(mpi_self == 0) {
+					ppp_pnm_write(output, kind, rows, columns, maxcolor, image);
+					printf("Load:\t\t%f\nScatter:\t%f\nReduction:\t%f\nScale:\t\t%f\nGather:\t\t%f\n",
+						t_load[measurepos], t_scatter[measurepos], t_reduction[measurepos], t_scale[measurepos], t_gather[measurepos]);
+				}
+			
+				/* MPI beenden */
+				MPI_Finalize();
+			break;}
+		
+			default:
+			break;
+		}
+	}
+
+	/*
+	 * Calc average of the t_measures with MEASURES as count... 
+	 */
+	 
 	
 	if(image != NULL) {
 	    free(image);
