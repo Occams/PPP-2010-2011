70e1f76cbf0a69556723a28c35aa744c76d34ee6
Daniel Watzinger <daniel.watzinger@gmail.com>
2011-01-15 15:15:38 +0100

    asd

--- Blatt05/image_encoder.c
+++ Blatt05/image_encoder.c
@@ -14,338 +14,332 @@
 
 
 enum impl_type {
-    IMPL_SEQ, IMPL_CPU, IMPL_GPU
+	IMPL_SEQ, IMPL_CPU, IMPL_GPU
 };
 
 double seconds() {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return (double)tv.tv_sec + ((double)tv.tv_usec)/1000000.0;
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return (double)tv.tv_sec + ((double)tv.tv_usec)/1000000.0;
 }  
 
 /*
- * Encode a frame using the CPU implementation.
- */
+* Encode a frame using the CPU implementation.
+*/
 static ppp_frame *encode(uint8_t *image, const ppp_image_info *info) {
-    ppp_frame *frame;
-    int16_t *blocks;
-    const int rows = info->rows;
-    const int columns = info->columns;
-    const enum ppp_image_format format = info->format;
+	ppp_frame *frame;
+	int16_t *blocks;
+	const int rows = info->rows;
+	const int columns = info->columns;
+	const enum ppp_image_format format = info->format;
 
-    /*
-     * max_enc_bytes is the maximal number of bytes needed 
-     * in any of the supported formats.
-     */
-    const int max_enc_bytes = max_encoded_length(rows*columns);
+	/*
+	* max_enc_bytes is the maximal number of bytes needed 
+	* in any of the supported formats.
+	*/
+	const int max_enc_bytes = max_encoded_length(rows*columns);
 
-    /*
-     * Allocate space for the macro blocks.
-     */
-    blocks = malloc(rows*columns * sizeof(int16_t));
-    if (blocks == NULL)
-        return NULL;
+	/*
+	* Allocate space for the macro blocks.
+	*/
+	blocks = malloc(rows*columns * sizeof(int16_t));
+	if (blocks == NULL)
+	return NULL;
 
-    /*
-     * Allocate a frame for the result.
-     */
-    frame = ppp_frame_alloc(max_enc_bytes);
-    if (frame == NULL) {
-        free(blocks);
-        return NULL;
-    }
+	/*
+	* Allocate a frame for the result.
+	*/
+	frame = ppp_frame_alloc(max_enc_bytes);
+	if (frame == NULL) {
+		free(blocks);
+		return NULL;
+	}
 
-    double t = seconds();
+	double t = seconds();
 
-    /*
-     * Convert the image to blocks (i.e., change the ordering of
-     * the pixels from row-wise to macro block order).
-     */
-    uint8_image_to_blocks(image, rows, columns, blocks);
+	/*
+	* Convert the image to blocks (i.e., change the ordering of
+	* the pixels from row-wise to macro block order).
+	*/
+	uint8_image_to_blocks(image, rows, columns, blocks);
 
-    /*
-     * Encode the blocks according to 'format'.
-     */
-    encode_frame(blocks, rows, columns, format, frame);
-    
-    t = seconds() - t;
-    printf("Duration: %.3f ms\n", t*1000);
+	/*
+	* Encode the blocks according to 'format'.
+	*/
+	encode_frame(blocks, rows, columns, format, frame);
+	
+	t = seconds() - t;
+	printf("Duration: %.3f ms\n", t*1000);
 
-    free(blocks);
+	free(blocks);
 
-    return frame;
+	return frame;
 }
 
 
 /*
- * Encode a frame using the OpenCL implementation.
- */
+* Encode a frame using the OpenCL implementation.
+*/
 static ppp_frame *encode_opencl(uint8_t *image, const ppp_image_info *info,
-                                bool use_gpu) {
+bool use_gpu) {
 
-    ppp_frame *frame;
-    const cl_uint rows = info->rows;
-    const cl_uint columns = info->columns;
-    const cl_uint format = info->format;
+	ppp_frame *frame;
+	const cl_uint rows = info->rows;
+	const cl_uint columns = info->columns;
+	const cl_uint format = info->format;
 
-    /*
-     * max_enc_bytes is the maximal number of bytes needed 
-     * in any of the supported formats.
-     */
-    const int max_enc_bytes = max_encoded_length(rows*columns);
+	/*
+	* max_enc_bytes is the maximal number of bytes needed 
+	* in any of the supported formats.
+	*/
+	const int max_enc_bytes = max_encoded_length(rows*columns);
 
-    cl_context context;
-    cl_device_id devid;
-    cl_command_queue queue;
-    cl_program program;
-    cl_kernel kernel;
-    cl_mem imageGPU, frameGPU, frameGPUComprIx;
-    cl_int res;
-
-    /* Set the work group size and global number of work items.
-     */
-    size_t work_dims = 2;
-    size_t global_work_size[] = { columns/8, rows/8 };
-    size_t local_work_size[] = {8,8};
+	cl_context context;
+	cl_device_id devid;
+	cl_command_queue queue;
+	cl_program program;
+	cl_kernel kernel;
+	cl_mem imageGPU, frameGPU;
+	cl_int res;
 
 
-    /* Allocate space for the result. */
-    frame = ppp_frame_alloc(max_enc_bytes);
-    if (frame == NULL)
-        return NULL;
+	/* Allocate space for the result. */
+	frame = ppp_frame_alloc(max_enc_bytes);
+	if (frame == NULL)
+		return NULL;
 
-    context = create_cl_context(use_gpu, &devid);
-    
-    /* Create a command queue (which allows timing) */
-    queue = clCreateCommandQueue(context, devid, CL_QUEUE_PROFILING_ENABLE,
-                                 &res);
-    if (queue == NULL)
-        error_and_abort("Could not create command queue", res);
-    
-    /* Allocate the buffer memory object for the input image.
-     * We request that the image (from the host) is copied to the device.
-     */
-    imageGPU = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-                              sizeof(*image)*rows*columns, (void *)image, &res);
-    if (res != CL_SUCCESS)
-        error_and_abort("Could not allocate imageGPU", res);
-        
-    /* Allocate the buffer memory object for the result.
-     * We need at most 'max_enc_bytes' to represent the result.
-     */
-    frameGPU = clCreateBuffer(context, CL_MEM_READ_WRITE,
-                              max_enc_bytes, NULL, &res);
-                              
-    if (res != CL_SUCCESS)
-        error_and_abort("Could not allocate frameGPU", res);
-        
-    if(info->format == PPP_IMGFMT_COMPRESSED_DCT) {
-        frameGPUComprIx = clCreateBuffer(context, CL_MEM_READ_WRITE,
-                              (rows*columns)/64, NULL, &res);
-                              
-        if (res != CL_SUCCESS)
-            error_and_abort("Could not allocate frameGPUComprIx", res);
-    }
+	context = create_cl_context(use_gpu, &devid);
+	
+	/* Create a command queue (which allows timing) */
+	queue = clCreateCommandQueue(context, devid, CL_QUEUE_PROFILING_ENABLE,
+		&res);
+	if (queue == NULL)
+		error_and_abort("Could not create command queue", res);
+	
+	/* Allocate the buffer memory object for the input image.
+	* We request that the image (from the host) is copied to the device.
+	*/
+	imageGPU = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+	sizeof(*image)*rows*columns, (void *)image, &res);
+	if (res != CL_SUCCESS)
+		error_and_abort("Could not allocate imageGPU", res);
+	
+	/* Allocate the buffer memory object for the result.
+	* We need at most 'max_enc_bytes' to represent the result.
+	*/
+	frameGPU = clCreateBuffer(context, CL_MEM_READ_WRITE,
+	max_enc_bytes, NULL, &res);
+	
+	if (res != CL_SUCCESS)
+		error_and_abort("Could not allocate frameGPU", res);
 
-    /*
-     * Load the OpenCL program from file "image_encoder_kernels.cl"
-     * and compile it.
-     */
-    program = build_program("image_encoder_kernels.cl", context, devid);
-    
-    /* Find kernel "encode_frame" in the compiled program. */
-    kernel = clCreateKernel(program, "encode_frame", NULL);
-    if (kernel == NULL) {
-        fprintf(stderr, "Could not create kernel 'encode_frame'.\n");
-        exit(1);
-    }
+	/*
+	* Load the OpenCL program from file "image_encoder_kernels.cl"
+	* and compile it.
+	*/
+	program = build_program("image_encoder_kernels2.cl", context, devid);
+	
+	/* Find kernel "blockwise_order" in the compiled program. */
+	kernel = clCreateKernel(program, "blockwise_order", NULL);
+	if (kernel == NULL) {
+		fprintf(stderr, "Could not create kernel 'encode_frame'.\n");
+		exit(1);
+	}
 
-    /* Set the arguments for the kernel invocation.
-     * We pass the pointers to the input image, the number of rows
-     * and columns, the format and the pointer to the result.
-     */
-    clSetKernelArg(kernel, 0, sizeof(cl_mem),  &imageGPU);
-    clSetKernelArg(kernel, 1, sizeof(cl_uint), &rows);
-    clSetKernelArg(kernel, 2, sizeof(cl_uint), &columns);
-    clSetKernelArg(kernel, 3, sizeof(cl_uint), &format);
-    clSetKernelArg(kernel, 4, sizeof(cl_mem),  &frameGPU);
-    clSetKernelArg(kernel, 5, sizeof(cl_mem),  &frameGPUComprIx);
+	/* Set the arguments for the kernel invocation.
+	* We pass the pointers to the input image, the number of rows
+	* and columns, the format and the pointer to the result.
+	*/
+	clSetKernelArg(kernel, 0, sizeof(cl_mem),  &imageGPU);
+	clSetKernelArg(kernel, 1, sizeof(cl_uint), &rows);
+	clSetKernelArg(kernel, 2, sizeof(cl_uint), &columns);
+	clSetKernelArg(kernel, 3, sizeof(cl_uint), &format);
+	clSetKernelArg(kernel, 4, sizeof(cl_mem),  &frameGPU);
+	
+	/* Set the work group size and global number of work items for blockwise_order kernel. */
+	size_t work_dims = 2;
+	size_t global_work_size[] = {columns, rows};
+	// size_t local_work_size[] = {8,8};
 
-    cl_event kernelEvent;
-    res = clEnqueueNDRangeKernel(queue, kernel, work_dims, NULL,
-                                 global_work_size, local_work_size,
-                                 0, NULL, &kernelEvent);
-    if (res != CL_SUCCESS)
-        error_and_abort("Could not enqueue kernel invocation", res);
+	cl_event kernelEvent;
+	cl_event kernelEvent_mm;
+	
+	res = clEnqueueNDRangeKernel(queue, kernel, work_dims, NULL,
+				global_work_size, NULL,
+				0, NULL, &kernelEvent);
+	if (res != CL_SUCCESS)
+		error_and_abort("Could not enqueue kernel invocation", res);
+	
+	if (info->format == PPP_IMGFMT_UNCOMPRESSED_DCT || info->format == PPP_IMGFMT_COMPRESSED_DCT) {
+		
+		/* Find kernel "mm" in the compiled program. */
+		kernel = clCreateKernel(program, "mm", NULL);
+		if (kernel == NULL) {
+			fprintf(stderr, "Could not create kernel 'mm'.\n");
+			exit(1);
+		}
 
+		/* Set the arguments for the kernel invocation.
+		* We pass the pointers to the input image, the number of rows
+		* add columns, the format and the pointer to the result.
+		*/
+		clSetKernelArg(kernel, 0, sizeof(cl_mem),  &imageGPU);
+		clSetKernelArg(kernel, 1, sizeof(cl_uint), &rows);
+		clSetKernelArg(kernel, 2, sizeof(cl_uint), &columns);
+		clSetKernelArg(kernel, 3, sizeof(cl_uint), &format);
+		clSetKernelArg(kernel, 4, sizeof(cl_mem),  &frameGPU);
 
-    /* If we do not compress, the result has the same size as the
-     * input (needs to be changed...).
-     */
-    size_t size;
-    if(info->format != PPP_IMGFMT_COMPRESSED_DCT) {
-        size = rows * columns;
-        frame->length = size;
-    } else {
-        size = max_enc_bytes;
-    }
+		/* Set the work group size and global number of work items for mm kernel. */
+		size_t global_work_size_mm [] = {columns, rows};
+		size_t local_work_size_mm [] = {8,8};
+		
+		res = clEnqueueNDRangeKernel(queue, kernel, work_dims, NULL,
+				global_work_size_mm, local_work_size_mm,
+				1, &kernelEvent, &kernelEvent_mm);
+				
+		if (res != CL_SUCCESS)
+			error_and_abort("Could not enqueue kernel invocation", res);	
+	}
 
-        
-    /* Copy the result from the device to the host. */
-    res = clEnqueueReadBuffer(queue, frameGPU, CL_TRUE, 0,
-                            size, frame->data, 0, NULL, NULL);
+	/* If we do not compress, the result has the same size as the
+	* input (needs to be changed...).
+	*/
+	size_t size;
+	size = rows * columns;
 
-    if (res != CL_SUCCESS)
-        error_and_abort("Could not enqueue buffer read", res);
-        
-    /* Compress encoded values by using the encoding index */
-    if(info->format == PPP_IMGFMT_COMPRESSED_DCT) {
-        int encix_size = (rows*columns)/64;
-        uint8_t encix[encix_size];
-        
-        /* Copy the encoding index from the device to the host. */
-        res = clEnqueueReadBuffer(queue, frameGPUComprIx, CL_TRUE, 0,
-                                  encix_size, encix, 0, NULL, NULL);
-        if (res != CL_SUCCESS)
-            error_and_abort("Could not enqueue buffer (frameGPUComprIx) read", res);
-        
-        frame->length = 0;
-        for(int i = 0; i < encix_size; i++) {
-            if(frame->length > 0) {
-                memcpy(&(frame->data[frame->length]), &(frame->data[i*96]), encix[i]);
-            }
-            frame->length += encix[i];
-        }
-    }
-        
-    res = clFinish(queue);
-    if (res != CL_SUCCESS)
-        error_and_abort("Error at clFinish", res);
+	/* Copy the result from the device to the host. */
+	res = clEnqueueReadBuffer(queue, frameGPU, CL_TRUE, 0,
+		size, frame->data, 0, NULL, NULL);
 
-    cl_ulong nanos = get_event_end_nanos(kernelEvent) -
-       get_event_start_nanos(kernelEvent);
-    printf("Duration: %.3f ms\n", nanos/1.0e6);
+	if (res != CL_SUCCESS)
+		error_and_abort("Could not enqueue buffer read", res);
+	
+	res = clFinish(queue);
+	if (res != CL_SUCCESS)
+		error_and_abort("Error at clFinish", res);
+	
+	cl_ulong end = info->format == PPP_IMGFMT_UNCOMPRESSED_DCT ? get_event_end_nanos(kernelEvent_mm) : get_event_end_nanos(kernelEvent);
+	cl_ulong nanos = end - get_event_start_nanos(kernelEvent);
+	printf("Duration: %.3f ms\n", nanos/1.0e6);
 
-    clReleaseMemObject(imageGPU);
-    clReleaseMemObject(frameGPU);
-    if(info->format == PPP_IMGFMT_COMPRESSED_DCT)
-        clReleaseMemObject(frameGPUComprIx);
-    clReleaseKernel(kernel);
-    clReleaseContext(context);
+	clReleaseMemObject(imageGPU);
+	clReleaseMemObject(frameGPU);
+	clReleaseKernel(kernel);
+	clReleaseContext(context);
 
-    return frame;
+	return frame;
 }
 
 
 static void usage(const char *progname) {
-  fprintf(stderr, "USAGE: %s -i IN -o OUT [-d | -c] [-z IMPL]\n", progname);
-  fprintf(stderr,
-	  "  -d: with DCT\n"
-	  "  -c: with DCT and compression\n"
-          "  -z: select implementation:\n"
-          "        seq: CPU sequential (default)\n"
-          "        cpu: CPU via OpenCL\n"
-          "        gpu: GPU via OpenCL\n"
-          "  -s: show encoder statistics (only with -c)\n"
-          "  -h: show this help\n"
-          "\n"
-	  );
+	fprintf(stderr, "USAGE: %s -i IN -o OUT [-d | -c] [-z IMPL]\n", progname);
+	fprintf(stderr,
+	"  -d: with DCT\n"
+	"  -c: with DCT and compression\n"
+	"  -z: select implementation:\n"
+	"        seq: CPU sequential (default)\n"
+	"        cpu: CPU via OpenCL\n"
+	"        gpu: GPU via OpenCL\n"
+	"  -s: show encoder statistics (only with -c)\n"
+	"  -h: show this help\n"
+	"\n"
+	);
 }
 
 int main(int argc, char *argv[]) {
-    enum pnm_kind kind;
-    enum ppp_image_format format;
-    int rows, columns, maxcolor;
-    uint8_t *image;
-    int option;
+	enum pnm_kind kind;
+	enum ppp_image_format format;
+	int rows, columns, maxcolor;
+	uint8_t *image;
+	int option;
 
-    char *infile, *outfile;
-    bool dct, compress, show_stats;
-    enum impl_type implementation;
+	char *infile, *outfile;
+	bool dct, compress, show_stats;
+	enum impl_type implementation;
 
-    init_qdct();
+	init_qdct();
 
-    infile = NULL;
-    outfile = NULL;
-    dct = false;
-    compress = false;
-    show_stats = false;
-    implementation = IMPL_SEQ;
-    while ((option = getopt(argc,argv,"cdhi:o:sz:")) != -1) {
-        switch(option) {
-        case 'd': dct = true; break;
-        case 'c': compress = true; break;
-        case 'i': infile = strdup(optarg); break;
-        case 'o': outfile = strdup(optarg); break;
-        case 's': show_stats = true; break;
-        case 'z':
-            if (strcmp(optarg, "seq") == 0) {
-                implementation = IMPL_SEQ;
-                break;
-            } else if (strcmp(optarg, "cpu") == 0) {
-                implementation = IMPL_CPU;
-                break;
-            } else if (strcmp(optarg, "gpu") == 0) {
-                implementation = IMPL_GPU;
-                break;
-            }
-            /* fall through */
-        case 'h':
-        default:
-            usage(argv[0]);
-            return 1;
-        }
-    }
-    
-    if (infile == NULL || outfile == NULL || (dct && compress)) {
-        usage(argv[0]);
-        return 1;
-    }
-    
-    if (dct)
-        format = PPP_IMGFMT_UNCOMPRESSED_DCT;
-    else if (compress)
-        format = PPP_IMGFMT_COMPRESSED_DCT;
-    else
-        format = PPP_IMGFMT_UNCOMPRESSED_BLOCKS;
-    
-    image = ppp_pnm_read(infile, &kind, &rows, &columns, &maxcolor);
-    
-    if (image != NULL) {
-        if (rows%8 != 0 || columns%8 != 0) {
-            fprintf(stderr, "Error: number of rows and columns must be "
-                    "multiples of 8\n");
-        } else if (kind == PNM_KIND_PGM) {
-            ppp_image_info info;
-            ppp_frame *frame;
-            info.rows = rows;
-            info.columns = columns;
-            info.format = format;
+	infile = NULL;
+	outfile = NULL;
+	dct = false;
+	compress = false;
+	show_stats = false;
+	implementation = IMPL_SEQ;
+	while ((option = getopt(argc,argv,"cdhi:o:sz:")) != -1) {
+		switch(option) {
+		case 'd': dct = true; break;
+		case 'c': compress = true; break;
+		case 'i': infile = strdup(optarg); break;
+		case 'o': outfile = strdup(optarg); break;
+		case 's': show_stats = true; break;
+		case 'z':
+			if (strcmp(optarg, "seq") == 0) {
+				implementation = IMPL_SEQ;
+				break;
+			} else if (strcmp(optarg, "cpu") == 0) {
+				implementation = IMPL_CPU;
+				break;
+			} else if (strcmp(optarg, "gpu") == 0) {
+				implementation = IMPL_GPU;
+				break;
+			}
+			/* fall through */
+		case 'h':
+		default:
+			usage(argv[0]);
+			return 1;
+		}
+	}
+	
+	if (infile == NULL || outfile == NULL || (dct && compress)) {
+		usage(argv[0]);
+		return 1;
+	}
+	
+	if (dct)
+	format = PPP_IMGFMT_UNCOMPRESSED_DCT;
+	else if (compress)
+	format = PPP_IMGFMT_COMPRESSED_DCT;
+	else
+	format = PPP_IMGFMT_UNCOMPRESSED_BLOCKS;
+	
+	image = ppp_pnm_read(infile, &kind, &rows, &columns, &maxcolor);
+	
+	if (image != NULL) {
+		if (rows%8 != 0 || columns%8 != 0) {
+			fprintf(stderr, "Error: number of rows and columns must be "
+			"multiples of 8\n");
+		} else if (kind == PNM_KIND_PGM) {
+			ppp_image_info info;
+			ppp_frame *frame;
+			info.rows = rows;
+			info.columns = columns;
+			info.format = format;
 
-            if (implementation == IMPL_SEQ)
-                frame = encode(image, &info);
-            else
-                frame = encode_opencl(image, &info,
-                                      implementation == IMPL_GPU);
+			if (implementation == IMPL_SEQ)
+			frame = encode(image, &info);
+			else
+			frame = encode_opencl(image, &info,
+			implementation == IMPL_GPU);
 
-            if (frame != NULL) {
-                if (show_stats && format == PPP_IMGFMT_COMPRESSED_DCT) {
-                    encoder_stats_init();
-                    encoder_stats(frame->data, frame->length);
-                    encoder_stats_print(stderr);
-                }
+			if (frame != NULL) {
+				if (show_stats && format == PPP_IMGFMT_COMPRESSED_DCT) {
+					encoder_stats_init();
+					encoder_stats(frame->data, frame->length);
+					encoder_stats_print(stderr);
+				}
 
-                if (ppp_image_write(outfile, &info, frame) != 0)
-                    fprintf(stderr, "could not write image\n");
-                free(frame);
-            } else
-                fprintf(stderr, "error while encoding\n");
-        } else
-            fprintf(stderr, "not a PGM image\n");
-        free(image);
-    } else
-        fprintf(stderr, "could not load image\n");
-    
-    return 0;
+				if (ppp_image_write(outfile, &info, frame) != 0)
+				fprintf(stderr, "could not write image\n");
+				free(frame);
+			} else
+			fprintf(stderr, "error while encoding\n");
+		} else
+		fprintf(stderr, "not a PGM image\n");
+		free(image);
+	} else
+	fprintf(stderr, "could not load image\n");
+	
+	return 0;
 }
 
---
+++ Blatt05/image_encoder_kernels2.cl
@@ -0,0 +1,223 @@
+/* Hey, Emacs, this file contains -*- c -*- code. */
+
+/*
+ * Define int16_t and uint8_t (so we can use the same types as
+ * in the host code).
+ */
+typedef short int16_t;
+typedef uchar uint8_t;
+
+enum ppp_image_format {
+    PPP_IMGFMT_UNCOMPRESSED_BLOCKS = 0,
+    PPP_IMGFMT_UNCOMPRESSED_DCT    = 1,
+    PPP_IMGFMT_COMPRESSED_DCT      = 2
+};
+
+/*
+ * OpenCL extension: allow printf() in kernels
+ * (AMD/ATI implementation only)
+ */
+#pragma OPENCL EXTENSION cl_amd_printf: enable
+
+/*
+ * Coefficients of the matrix A^tr to be used in DCT computation
+ */
+constant float dct_coeffs_tr[64] = {
+    0.35355338f,  0.49039263f ,  0.46193978f ,  0.4157348f   ,
+                  0.35355338f ,  0.2777851f  ,  0.19134171f  , 9.754512e-2f,
+    0.35355338f,  0.4157348f  ,  0.19134171f , -9.754516e-2f ,
+                 -0.35355338f , -0.49039266f , -0.46193978f  , -0.277785f,
+    0.35355338f,  0.2777851f  , -0.19134176f , -0.49039266f  ,
+                 -0.35355332f ,  9.754521e-2f, 0.46193978f   , 0.41573468f,
+    0.35355338f,  9.754512e-2f, -0.46193978f , -0.277785f    ,
+                  0.3535535f  ,  0.41573468f , -0.19134195f  , -0.4903926f,
+    0.35355338f, -9.754516e-2f, -0.46193978f ,  0.2777852f   ,
+                  0.35355338f , -0.4157349f  , -0.19134149f  , 0.4903927f,
+    0.35355338f, -0.27778518f , -0.19134156f ,  0.49039263f  ,
+                 -0.35355362f , -9.754511e-2f,  0.46193966f  , -0.4157348f,
+    0.35355338f, -0.41573483f ,  0.1913418f  ,  9.7545035e-2f,
+                 -0.35355327f ,  0.49039266f , -0.46193987f  , 0.27778557f,
+    0.35355338f, -0.49039266f ,  0.46193978f , -0.4157349f   ,
+                  0.3535534f ,  -0.27778542f ,  0.19134195f  , -9.754577e-2f
+};
+
+/*
+ * Permutation of 64 values in a macro block.
+ * Used in qdct_block() and iqdct_block().
+ */
+constant int permut[64] = {
+     0,  1,  5,  6, 14, 15, 27, 28,
+     2,  4,  7, 13, 16, 26, 29, 42,
+     3,  8, 12, 17, 25, 30, 41, 43,
+     9, 11, 18, 24, 31, 40, 44, 53,
+    10, 19, 23, 32, 39, 45, 52, 54,
+    20, 22, 33, 38, 46, 51, 55, 60,
+    21, 34, 37, 47, 50, 56, 59, 61,
+    35, 36, 48, 49, 57, 58, 62, 63
+};
+
+/*
+ * Quantization factors for the results of the DCT
+ * of a macro block. Used in qdct_block() and iqdct_block().
+ */
+constant int quantization_factors[64] = {
+    16,  11,  10,  16,  24,  40,  51,  61,
+    12,  12,  14,  19,  26,  58,  60,  55,
+    14,  13,  16,  24,  40,  57,  69,  56,
+    14,  17,  22,  29,  51,  87,  80,  62,
+    18,  22,  37,  56,  68, 109, 103,  77,
+    24,  35,  55,  64,  81, 104, 113,  92,
+    49,  64,  78,  87, 103, 121, 120, 101,
+    72,  92,  95,  98, 112, 100, 103,  99
+};
+
+/*
+ * Computes M = A*M*A_tr
+ */
+void mm_tr(constant float A_tr[64], float *M) {
+    float AM[64];
+
+    /* Compute AM = A*M */
+    for (int y=0; y<8; y++) {
+        for (int x=0; x<8; x++) {
+            float acc = 0.0f;
+            for (int i=0; i<8; i++)
+                acc += A_tr[i*8+y] * M[8*i+x]; /* A_tr must be addressed so that we get A */
+            AM[y*8+x] = acc;
+        }
+    }
+
+    /* Compute M = (AM)*A_tr */
+    for (int y=0; y<8; y++) {
+        for (int x=0; x<8; x++) {
+            float acc = 0.0f;
+            for (int i=0; i<8; i++)
+                acc += AM[y*8+i] * A_tr[8*i+x]; /* A_tr must be addressed so that we get A */
+            M[y*8+x] = acc;
+        }
+    }
+}
+
+/*
+ * Return the value for the first nibble of the
+ * encoding of 'val' (val /= 0).
+ */
+uint8_t first_nibble(int16_t val) {
+    if (val == 1)
+        return 0x8;
+    else if (val == 2)
+        return 0x9;
+    else if (val == -1)
+        return 0xA;
+    else if (val == -2)
+        return 0xB;
+    else if (val >= 19)
+        return 0xE;
+    else if (val <= -19)
+        return 0xF;
+    else if (val <= -3)
+        return 0xD;
+    else
+        return 0xC;
+}
+
+/*
+ * Compress the given input values (in 'input') storing
+ * the compressed data in 'codes'. 'n_values' values are read from
+ * 'input'. Returns the number of bytes used in 'codes' (i.e.,
+ * the length of the compressed data in bytes).
+ */
+int compress_data(const int16_t *input, global uint8_t *codes) {
+    const int n_values = 64;
+    uint8_t nibbles[3*64];
+
+    /* Walk through the values. */
+    int zeros = 0, pos = 0;
+    for (int i=0; i<n_values; i++) {
+        int16_t val = input[i];
+        if (val == 0)
+            zeros++;
+        else {
+            /* When we meet a non-zero value, we output an appropriate
+             * number of codes for the zeros preceding the current
+             * non-zero value.
+             */
+            int16_t absval = val < 0 ? -val : val;
+            while (zeros > 0) {
+                int z = zeros > 7 ? 7 : zeros;
+                nibbles[pos++] = z;
+                zeros -= z;
+            }
+            nibbles[pos++] = first_nibble(val);
+            if (absval >= 19) {
+                uint8_t code = absval - 19;
+                nibbles[pos++] = code >> 4;
+                nibbles[pos++] = code & 0xF;
+            } else if (absval >= 3) {
+                nibbles[pos++] = absval - 3;
+            }
+        }
+    }
+
+    /* When the sequence ends with zeros, terminate the
+     * sequence with 0x0.
+     */
+    if (zeros > 0)
+        nibbles[pos++] = 0;
+    
+    /* Add a nibble 0x0 if the number of nibbles in the code
+     * is odd.
+     */
+    if ((pos & 1) != 0)
+        nibbles[pos++] = 0;
+    
+    /*
+     * Pack the nibbles into bytes. The first nibble of each
+     * pair of nibbles goes into the upper half of the byte.
+     */
+    for (int i=0; i<pos/2; i++)
+        codes[i] = (nibbles[2*i] << 4) | nibbles[2*i+1];
+
+    /* Return the number of bytes used. */
+    return pos/2;
+}
+
+kernel void blockwise_order(global uint8_t *image,
+                         uint rows, uint columns, uint format,
+                         global uint8_t *frame) {
+    int col = get_global_id(0);
+    int row = get_global_id(1);
+	int b_col = col / 8;
+	int b_row = row / 8;
+	int b_col_offset = col & 7;
+	int b_row_offset = row & 7;
+	
+	/* Blockwise order */
+	int idx = b_row * 8 * columns + b_col * 64 + b_row_offset * 8 + b_col_offset;
+	frame[idx] = image[row * columns + col] - 128;
+}
+
+kernel void mm(global uint8_t *image,
+                         uint rows, uint columns, uint format,
+                         global uint8_t *frame) {
+	int col = get_global_id(0);
+    int row = get_global_id(1);
+	int b_col = col / 8;
+	int b_row = row / 8;
+	int b_col_offset = col & 7;
+	int b_row_offset = row & 7;
+	int b_offset = b_row * 8 * columns + b_col * 64;
+	int start_idx = b_offset + b_row_offset * 8;
+	
+	/* M*A_tr */
+	float res = 0.0f;
+	
+	for (int i = 0; i < 8; i++)
+		res+= frame[start_idx + i] * dct_coeffs_tr[i * 8 + b_col_offset];
+		
+	/* A*M */
+	
+	/* Quantization, permutation */
+	barrier(CLK_GLOBAL_MEM_FENCE);
+	frame[b_offset + permut[b_row_offset * 8 + b_col_offset]] = rint(res / quantization_factors[b_row_offset * 8 + b_col_offset]);
+}
